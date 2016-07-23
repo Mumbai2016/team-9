17bb9b251c8dd7be4a5e365fe7a3e6112270dd4a
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-07-23 22:54:23 +0530

    added level layer

--- source/dreamadream/src/main/java/org/dreamadream/controller/LevelController.java
+++ source/dreamadream/src/main/java/org/dreamadream/controller/LevelController.java
@@ -1,27 +1,24 @@
 package org.dreamadream.controller;
 
-import java.io.IOException;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.dreamadream.service.impl.LevelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 
 public class LevelController {
 	
-	@RequestMapping(
-			value = "/get-levels", 
-			method = RequestMethod.GET)
+
+	@Autowired
+	private LevelService levelService;
+	
+	
+	@RequestMapping(value = "/get-levels", method = RequestMethod.GET)
 	public @ResponseBody String getLevels(){
 	
+		System.out.println(levelService.getLevel("102"));
+		
 		
 		return "";
 	}
---
+++ source/dreamadream/src/main/java/org/dreamadream/model/Level.java
@@ -0,0 +1,69 @@
+package org.dreamadream.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "level")
+public class Level {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	@Column(name = "user_id")
+	private String userId;
+	
+	@Column(name = "level")
+	private String level;
+	
+	@Column(name = "level_date_of_joining")
+	private String levelDateOfJoining;
+	
+	@Column(name = "level_end_date")
+	private String levelEndDate;
+	
+	@Column(name = "ngo_comments")
+	private String ngoComments;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getLevelDateOfJoining() {
+		return levelDateOfJoining;
+	}
+	public void setLevelDateOfJoining(String levelDateOfJoining) {
+		this.levelDateOfJoining = levelDateOfJoining;
+	}
+	public String getLevelEndDate() {
+		return levelEndDate;
+	}
+	public void setLevelEndDate(String levelEndDate) {
+		this.levelEndDate = levelEndDate;
+	}
+	public String getNgoComments() {
+		return ngoComments;
+	}
+	public void setNgoComments(String ngoComments) {
+		this.ngoComments = ngoComments;
+	}
+		
+	
+	@Override
+	public String toString(){
+		return "id="+userId;
+	}
+	  
+}
+
