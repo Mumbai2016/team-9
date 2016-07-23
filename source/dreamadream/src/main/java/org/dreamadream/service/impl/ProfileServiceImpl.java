bd618906c25a0161015cf576604ef315a04a52d7
prachivaity <vaity.prachi.13it1019@gmail.com>
2016-07-23 23:18:53 +0530

    merging prach 1

---
+++ source/dreamadream/src/main/java/org/dreamadream/service/impl/ProfileServiceImpl.java
@@ -0,0 +1,43 @@
+package org.dreamadream.service.impl;
+
+import org.dreamadream.beans.UserDetailsBean;
+import org.dreamadream.dao.ProfileDao;
+import org.dreamadream.model.UserDetails;
+import org.dreamadream.service.ProfileService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("profileService")
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+public class ProfileServiceImpl implements ProfileService {
+
+	@Autowired
+	private ProfileDao profileDAO;
+	
+	//@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+	public UserDetailsBean getProfile(UserDetailsBean userDetailsBean) {
+		//Profile result = profileDAOImpl.getProfile(user);
+		
+			
+			UserDetails userDetails=new UserDetails();
+			userDetails.setUserId(userDetailsBean.getUserId());
+			
+			
+			UserDetails result = profileDAO.getProfile(userDetails);
+			
+			
+			if(result == null){
+				System.out.println("Returned NULL");
+			}
+			
+			BeanUtils.copyProperties(result,userDetailsBean);
+			
+			
+			return userDetailsBean;
+		
+	}
+}
