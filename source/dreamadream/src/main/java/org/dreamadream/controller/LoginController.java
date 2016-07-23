ed5813215fa3f72f956b5e4e8bfd8e5c143a3f12
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-07-23 20:15:36 +0530

    login stage 1 ready

--- source/dreamadream/src/main/java/org/dreamadream/controller/LoginController.java
+++ source/dreamadream/src/main/java/org/dreamadream/controller/LoginController.java
@@ -1,6 +1,7 @@
 package org.dreamadream.controller;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
@@ -71,5 +74,42 @@ public class LoginController {
 		return "";
 	}
 	*/
+	
+	@RequestMapping(
+			value = "/login", 
+			method = RequestMethod.GET)
+	public ModelAndView login(HttpServletRequest request, HttpServletResponse response) {
+			
+		
+			return new ModelAndView("login");
+	}
+	
+	@RequestMapping(
+			value = "/login-submit", 
+			method = RequestMethod.POST)
+	public ModelAndView loginSubmit(
+			HttpServletRequest request, HttpServletResponse response,
+			@RequestParam Map<String, String> r)  {
+		
+		ModelAndView view = null;
+		
+		if(r.get("username").equals("student")
+				&& r.get("password").equals("123")){
+			
+			view = new ModelAndView("student");
+		}
+		else if(r.get("username").equals("student")
+				&& r.get("password").equals("123")){
+			view = new ModelAndView("admin");
+		}
+		
+		return view;
+	}
+	
+	
+	
+	
+	
+	
 
 }
